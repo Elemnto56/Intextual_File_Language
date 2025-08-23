63b1d81e7b953b4dfb35a712ed115d0a2943d3eb
Jaylon Christy <jaylon_christy@yahoo.com>
2025-08-23 00:45:45 -0500

    done with playground

--- itx-docs/docs/playground/script.js
+++ itx-docs/docs/playground/script.js
@@ -1,7 +1,3 @@
-window.addEventListener("load", function () {
-    console.log("Page has loaded")
-})
-
 function EnableLight() {
     let x = document.getElementById("bd")
     let y = document.getElementById("bt")
@@ -18,11 +14,11 @@ function EnableLight() {
 function Run() {
     let x = document.getElementById("code-input")
 
-    fetch("http://localhost:8080/api/send", {
+    fetch("https://api.codekeg.dev/run-code", {
         method: "POST",
         headers:  {"Content-Type": "application/json"},
         body: JSON.stringify({ data: x.value })
     })
     .then(res => res.json())
-    .then(response => document.getElementById("code-output").value = response.message)
+    .then(response => { console.log("We have recieved: ", response.message), document.getElementById("code-output").value = response.message})
 }
